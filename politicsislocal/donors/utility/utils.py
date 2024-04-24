5d0bcb2dd2d377085bd1dafaa46da2b472c1223d
Art Mills <art@hamcois.com>
2024-04-24 01:35:41 -0500

    None check

--- politicsislocal/donors/utility/utils.py
+++ politicsislocal/donors/utility/utils.py
@@ -2,6 +2,11 @@ import random
 import string
 
 def randomize_letters_and_numbers(value):
+    # Check if the input value is None and return it immediately if so
+    if value is None:
+        return value
+
+    # Proceed with randomization if the value is not None
     randomized = ''.join(
         random.choice(string.ascii_letters) if c.isalpha() else 
         random.choice(string.digits) if c.isdigit() else 
